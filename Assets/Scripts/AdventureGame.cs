5943fd732a440ed7e10b5ebce7d8ef5eee8bc5f3
McIntyre <66914497+MecIntyre@users.noreply.github.com>
2021-06-08 13:44:37 +0200

    SecretAdventureGame
    It is a short and small textbased adventuregame, about a hero moving through a forest.

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text TextComponent;
+    [SerializeField] State startingState;
+
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        TextComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        for (int index = 0; index < nextStates.Length; index++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + index))
+            {
+                state = nextStates[index];
+            }
+        }
+        TextComponent.text = state.GetStateStory();
+    }
+}
